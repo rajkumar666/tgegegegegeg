fbe0ca77864bcfd61390dff083e175d7ebbac47d
rajkumar666 <rajkum3979@gmail.com>
2017-07-17 09:46:35 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,20 @@
+# tgegegegegeg
+import java.io.*;
+import java.util.*;
+public class Main
+{
+public static void main(String [] args)
+{
+int n1,n2,n3,i,count++;
+Scanner sc=new Scanner(System.in);
+n1=sc.nextInt();
+n2=sc.nextInt();
+n3=sc.nextInt();
+for(i=2;i<count;i++)
+{
+n3=n1+n2;
+System.out.println(""+n3);
+n1=n2;
+n2=n3;
+}}}
+
